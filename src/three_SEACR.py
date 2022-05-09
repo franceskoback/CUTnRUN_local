08619c12c5578d1c82e9a91024a95b5ab296b534
franceskoback <31418742+franceskoback@users.noreply.github.com>
2022-05-09 11:43:07 -0700

    Update three_SEACR.py

--- src/three_SEACR.py
+++ src/three_SEACR.py
@@ -13,6 +13,7 @@ output_folder = sys.argv[3] # Output folder to store results and resulting bash 
 igg_control = sys.argv[4] # "y" or "n" if there is an igg control -- todo: "n" will become numberic threshold to return top n fraction of peaks 
 
 
+threshold = 0.1
 # create output directories if don't exist
 if not os.path.exists(output_folder):
 	os.makedirs(output_folder)
@@ -73,9 +74,16 @@ for count in range(len(nuclei_bedgraphs)):
     output_script.write("\n")
     prefix = nuclei_bedgraphs[count].rsplit('/', 1)[-1].rsplit('.')[0]
     current_command= nuclei_bedgraphs[count]
-    output_command = "bash " +seacr_path+ " " +str(nuclei_bedgraphs[count])+ " " +str(igg_bedgraphs[0])+ " non relaxed " + str(prefix)
-    output_script.write(output_command)
-    output_script.write("\n")
+    if igg_control=="y":
+        output_command = "bash " +seacr_path+ " " +str(nuclei_bedgraphs[count])+ " " +str(igg_bedgraphs[0])+ " non relaxed " + str(prefix)
+        output_script.write(output_command)
+        output_script.write("\n")
+    else: 
+        output_command = "bash " +seacr_path+ " " +str(nuclei_bedgraphs[count])+ " " +str(threshold)+ " non relaxed " + str(prefix)
+        output_script.write(output_command)
+        output_script.write("\n")
+        
+    
 
 output_script.write("\n\n")
 
@@ -86,9 +94,14 @@ for count in range(len(cell_bedgraphs)):
     output_script.write("\n")
     prefix = cell_bedgraphs[count].rsplit('/', 1)[-1].rsplit('.')[0]
     current_command= cell_bedgraphs[count]
-    output_command = "bash " +seacr_path+ " " +str(cell_bedgraphs[count])+ " " +str(igg_bedgraphs[0])+ " non relaxed " + str(prefix)
-    output_script.write(output_command)
-    output_script.write("\n")
+    if igg_control=="y":
+        output_command = "bash " +seacr_path+ " " +str(cell_bedgraphs[count])+ " " +str(igg_bedgraphs[0])+ " non relaxed " + str(prefix)
+        output_script.write(output_command)
+        output_script.write("\n")
+    else: 
+        output_command = "bash " +seacr_path+ " " +str(cell_bedgraphs[count])+ " " +str(threshold)+ " non relaxed " + str(prefix)
+        output_script.write(output_command)
+        output_script.write("\n")
     
 output_script.write("\n\n")
 
@@ -99,10 +112,15 @@ for count in range(len(unfixed_bedgraphs)):
     output_script.write("\n")
     prefix = unfixed_bedgraphs[count].rsplit('/', 1)[-1].rsplit('.')[0]
     current_command= cell_bedgraphs[count]
-    output_command = "bash " +seacr_path+ " " +str(unfixed_bedgraphs[count])+ " " +str(igg_bedgraphs[0])+ " non relaxed " + str(prefix)
-    output_script.write(output_command)
-    output_script.write("\n")
-    
+    if igg_control=="y":
+        output_command = "bash " +seacr_path+ " " +str(unfixed_bedgraphs[count])+ " " +str(igg_bedgraphs[0])+ " non relaxed " + str(prefix)
+        output_script.write(output_command)
+        output_script.write("\n")
+    else: 
+        output_command = "bash " +seacr_path+ " " +str(unfixed_bedgraphs[count])+ " " +str(threshold)+ " non relaxed " + str(prefix)
+        output_script.write(output_command)
+        output_script.write("\n")
+
 output_script.write("\n\n")    
     
 for count in range(len(nuclei_bedgraphs)):
@@ -112,9 +130,14 @@ for count in range(len(nuclei_bedgraphs)):
     output_script.write("\n")
     prefix = nuclei_bedgraphs[count].rsplit('/', 1)[-1].rsplit('.')[0]
     current_command= nuclei_bedgraphs[count]
-    output_command = "bash " +seacr_path+ " " +str(nuclei_bedgraphs[count])+ " " +str(igg_bedgraphs[0])+ " non stringent " + str(prefix)
-    output_script.write(output_command)
-    output_script.write("\n")
+    if igg_control=="y":
+        output_command = "bash " +seacr_path+ " " +str(nuclei_bedgraphs[count])+ " " +str(igg_bedgraphs[0])+ " non stringent " + str(prefix)
+        output_script.write(output_command)
+        output_script.write("\n")
+    else: 
+        output_command = "bash " +seacr_path+ " " +str(nuclei_bedgraphs[count])+ " " +str(threshold)+ " non stringent " + str(prefix)
+        output_script.write(output_command)
+        output_script.write("\n")
 
 output_script.write("\n\n")
 
@@ -125,9 +148,14 @@ for count in range(len(cell_bedgraphs)):
     output_script.write("\n")
     prefix = cell_bedgraphs[count].rsplit('/', 1)[-1].rsplit('.')[0]
     current_command= cell_bedgraphs[count]
-    output_command = "bash " +seacr_path+ " " +str(cell_bedgraphs[count])+ " " +str(igg_bedgraphs[0])+ " non stringent " + str(prefix)
-    output_script.write(output_command)
-    output_script.write("\n")
+    if igg_control=="y":
+        output_command = "bash " +seacr_path+ " " +str(cell_bedgraphs[count])+ " " +str(igg_bedgraphs[0])+ " non stringent " + str(prefix)
+        output_script.write(output_command)
+        output_script.write("\n")
+    else: 
+        output_command = "bash " +seacr_path+ " " +str(cell_bedgraphs[count])+ " " +str(threshold)+ " non stringent " + str(prefix)
+        output_script.write(output_command)
+        output_script.write("\n")
     
 output_script.write("\n\n")
 
@@ -138,9 +166,14 @@ for count in range(len(unfixed_bedgraphs)):
     output_script.write("\n")
     prefix = unfixed_bedgraphs[count].rsplit('/', 1)[-1].rsplit('.')[0]
     current_command= cell_bedgraphs[count]
-    output_command = "bash " +seacr_path+ " " +str(unfixed_bedgraphs[count])+ " " +str(igg_bedgraphs[0])+ " non stringent " + str(prefix)
-    output_script.write(output_command)
-    output_script.write("\n")
+    if igg_control=="y":
+        output_command = "bash " +seacr_path+ " " +str(unfixed_bedgraphs[count])+ " " +str(igg_bedgraphs[0])+ " non stringent " + str(prefix)
+        output_script.write(output_command)
+        output_script.write("\n")
+    else: 
+        output_command = "bash " +seacr_path+ " " +str(unfixed_bedgraphs[count])+ " " +str(threshold)+ " non stringent " + str(prefix)
+        output_script.write(output_command)
+        output_script.write("\n")
 
 output_script.write("\n\n")
 
