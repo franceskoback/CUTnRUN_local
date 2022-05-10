5e68b8420db696ddf6f6cd0ca1566c8beef7b13d
franceskoback <31418742+franceskoback@users.noreply.github.com>
2022-05-09 18:55:12 -0700

    Update five_HomerMotifs.py

--- src/five_HomerMotifs.py
+++ src/five_HomerMotifs.py
@@ -4,19 +4,28 @@ from os import listdir
 import sys
 
 # read in input arguments that are required
-peaks_dir				= sys.argv[1] # where is peak files are located <path>/03_peak_calling_v1beds
-beds_suffix	= sys.argv[2] # where are the normalized bed_files homerMotif.bed
-output_base_dir			= sys.argv[3] # base directory where to dump the identified motifs <path>/05_HomerMotifs
+peaks_dir				= sys.argv[1] # where peak files are located /results/three_calledpeaks
+output_base_dir			= sys.argv[2] # where to put results, ie /results/five_HomerMotifs
 #homer_path= sys.argv[4] # path to Homer ie /path/to/Homer
 
 # Move to the folder with the saved bam and stats files
-os.chdir( peaks_dir )
+path = str(peaks_dir)+"/relaxed"
+print(path)
+os.chdir(path)
+
 
 # figure out which are the nuclear files, which are the regular files
 peak_beds = []
 for file_name in listdir():
-	if file_name.endswith(beds_suffix):
-		peak_beds.append(peaks_dir+ "/" +file_name)
+	if file_name.endswith("relaxed.bed"):
+		peak_beds.append(peaks_dir+ "/relaxed/" +file_name)
+  
+path = str(peaks_dir)+"/stringent"
+os.chdir(path)
+for file_name in listdir():
+    if file_name.endswith("stringent.bed"):
+        peak_beds.append(peaks_dir+ "/stringent/" +file_name)
+        
 # sort alphabetically
 peak_beds.sort()
 
@@ -33,10 +42,7 @@ os.chdir( output_directory )
 # save commands to the output script
 script_name = "five_cut_n_run_homer_motifs_v1beds.sh"
 output_script = open( script_name, 'w' )
-for sample_counter in range(len(peak_beds)):
-    if not os.path.exists(output_directory+peak_beds[sample_counter].split("/")[-1].split("_R1")[0]):
-        os.makedirs(output_directory+peak_beds[sample_counter].split("/")[-1].split("_R1")[0])
-        #print(output_directory+peak_beds[sample_counter].split("/")[-1].split("_R1")[0])
+
 
 # for each file re-arrange to make in the format for Homer to call peaks
 for sample_counter in range(len(peak_beds)):
@@ -44,7 +50,7 @@ for sample_counter in range(len(peak_beds)):
 	output_command = "echo \"Running Motif Discovery on " +peak_beds[sample_counter].split("/")[-1].split("_R1")[0]+ "\""
 	output_script.write(output_command)
 	output_script.write("\n")
-	output_command = "cd " + output_base_dir + "/" + peak_beds[sample_counter].split("/")[-1].split("_R1")[0]
+	output_command = "cd " + output_base_dir
 	output_script.write(output_command)
 	output_script.write("\n")
 	# call Homer motif discovery
